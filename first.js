3bde3d791469e0e5fbdc6b7dc3c805bde09d034d
Ghtejasvi <tejasvighoniya2610@gmail.com>
2023-03-13 16:52:18 +0530

    first.js

---
+++ first.js
@@ -0,0 +1,77 @@
+// console.log("hello");
+
+// --------------Arithmetic operator--------------
+// console.log(10+10)
+
+// var a='30';
+// var b='20';
+// console.log(typeof(a));      //string
+// console.log(typeof(b));      //string
+// console.log(a+b);            //3020
+// console.log(a-b);            //10
+// console.log(a*b);            //600
+// console.log(a/b);            //1.5
+// console.log(a%b);            //10
+// console.log(+a+ +b);         //50
+
+// --------------Relational operator--------------
+
+// var a=30;
+// var b=20;
+// console.log(a>b);     //true
+// console.log(a<b);     //false
+// console.log(a>=b);     //true
+// console.log(a<=b);     //false 
+// console.log(a!=b);      //true
+// console.log(a==b);      //false
+
+// -------------------var------------------
+// var a=10;
+// console.log(a);
+// {
+//     var a=100;
+//     console.log(a);
+// }
+// console.log(a);
+
+// -------------------let------------------
+// let a=10;
+// console.log(a);
+// {
+//     let a=100;
+//     console.log(a);
+// }
+// console.log(a);
+
+// -------------------const------------------
+// const a=10;
+// console.log(a);
+// {
+//     const a=100;
+//     console.log(a);
+// }
+// console.log(a);
+
+// -------------------example------------------
+// let a=10;
+// let b='s';
+// let c;
+// c=a*b;
+// console.log(c);        //NaN = Not a Number
+// console.log(typeof(c));     //Number
+
+// ---------------------for--------------------
+// for(var i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+
+// ----------------------------
+// for(var i=1;i<=5;i++)
+// {
+//     for(var j=1;j<=i;j++)
+//     {
+//         console.log(i);
+//     }
+//     console.log("\n"); 
+// }
